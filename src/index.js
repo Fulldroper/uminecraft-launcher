04359f6485879204d9df27eed05aa8d9d3ae937b
CIHIKU <gewpm10@gmail.com>
2023-06-26 03:54:17 -0400

    Make some rape on code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import './assets/styles/fonts.css'
+import './assets/styles/reset.css'
 import App from './App/App';
 import * as serviceWorker from "./serviceWorker";
 import {UIContextProvider} from "./store/ui-context";
